997c3062c03c7ca62e37c74fc17e3b3b15407e43
Eka <ekananda@ti.ukdw.ac.id>
2021-11-12 11:51:53 +0800

    3__B_71210723

---
+++ 3_B_71210723.py
@@ -0,0 +1,13 @@
+#data
+nilai1 =int(input("Nilai 1 : "))
+nilai2 =int(input("Nilai 2 : "))
+nilai3 =int(input("Nilai 3 : "))
+total = nilai1+nilai2+nilai3
+rata = total/3
+
+#proses
+k1 = "Rata-rata dari"
+k2 = "adalah"
+
+#print
+print("{} {}+{}+{} {}".format(k1, nilai1, nilai2, nilai3, k2), rata )
